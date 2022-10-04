ce881037a810fb8dcf5ea8b557fa75e4a78ac4a4
mateusdata <mateuspele2015@gmail.com>
2022-10-04 17:58:15 -0300

    Adicionando links

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,20 @@ import Aluguel from "./componets/aluguel/aluguel";
 import Horas from "./componets/horas/horas";
 import Gotas from "./componets/remedio/gotas";
 import Hotel from "./componets/hotel/hotel";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> <p>Exercise list</p> </header>
+      <header className="App-header"> <p>Exercise list</p> 
+      <div id="redes">
+      <a href="https://www.instagram.com/mateus.data/" target="_blank" rel="noreferrer"><InstagramIcon style={{color:"#bb2b73"}}/></a>
+      <a href="https://www.linkedin.com/in/mateusdata/" target="_blank" rel="noreferrer"><LinkedInIcon style={{color:"#0961b8"}}/></a>
+      <a href="https://github.com/mateusdata" target="_blank" rel="noreferrer"><GitHubIcon style={{color:"#f2f2f2"}} /></a>
+      </div>
+      </header>
       <div className="matematica">
         <Dobro />
         <Trapezio />
