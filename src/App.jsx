bc031a795d945094751c7b999c72f69c25b7ac8a
mateusdata <mateuspele2015@gmail.com>
2022-10-05 12:34:20 -0300

    Ajustando o menu

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,23 @@ import Footer from "./componets/footer/footer";
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { useState } from "react";
+
 function App() {
+  const [menu, setMenu] =useState(null);
+
+  function menuActive() {
+    if(menu){
+      setMenu(false);
+    }
+    else{
+      setMenu(true);
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header"><MenuIcon id="menu"/> <p>Exercise list</p> 
+      <header className="App-header"><button onClick={menuActive} id="botaoMenu"><MenuIcon id="menu"/></button><p>Exercise list</p> 
       <div id="redes">
       <a href="https://www.instagram.com/mateus.data/" target="_blank" rel="noreferrer"><InstagramIcon style={{color:"#bb2b73"} } /></a>
       <a href="https://www.linkedin.com/in/mateusdata/" target="_blank" rel="noreferrer"><LinkedInIcon style={{color:"#0961b8"}}/></a>
@@ -24,6 +37,11 @@ function App() {
       </div>
       </header>
       <div className="matematica">
+        { menu ? <div className="divmenu">
+         <p>Menu</p>
+         <p>Sobre</p>
+         <p>Contribuir</p>
+      </div>: null}
         <Dobro />
         <Trapezio />
         <Aluguel />
